90b7aa877186bf3aed0a2d7c905492ce37fb2290
Jon <jharder@kipsu.com>
2023-12-19 11:18:35 -0600

    Clean up last of init.el code

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -3,24 +3,4 @@
   "config.org"
   user-emacs-directory))
 
-;;; temp function for hilton work
-
-;; Http
-(straight-use-package 'restclient)
-(add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
-
-
-;; Reading
-;;; Epub support
-(straight-use-package 'nov)
-(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
-
-;; miscellanious functions
-(defun masteringemacs ()
-  "Open the masteringemacs epub manual."
-  (interactive)
-  ;; ensure the nov package is installed
-  (require 'nov)
-  (find-file "~/Dropbox/Emacs/mastering-emacs-v4.epub"))
-
 (put 'dired-find-alternate-file 'disabled nil)
