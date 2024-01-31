0f1eae72ae0f1dbe236abd39b5d024da95f807de
Jon <jharder@kipsu.com>
2024-01-31 13:50:17 -0600

    Move epa code into gpg module

--- emacs/.config/emacs/my-modules/my-gpg.el
+++ emacs/.config/emacs/my-modules/my-gpg.el
@@ -2,4 +2,9 @@
 
 (setq epg-pinentry-mode 'loopback)
 
+(evil-define-key 'normal epa-key-list-mode-map
+  (kbd "TAB") #'forward-button
+  (kbd "m") #'epa-mark-key
+  (kbd "u") #'epa-unmark-key)
+
 (provide 'my-gpg)
