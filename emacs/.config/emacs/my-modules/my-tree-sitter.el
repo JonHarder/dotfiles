0af63079b40a3b6a79cfe3a5870adb35d87c7a28
Jon <jharder@kipsu.com>
2024-04-10 16:27:31 -0500

    emacs: add tree-sitter module

---
+++ emacs/.config/emacs/my-modules/my-tree-sitter.el
@@ -0,0 +1,12 @@
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+
+(setq treesit-language-source-alist
+      '((rust "https://github.com/tree-sitter/tree-sitter-rust")))
+
+(setq treesit-load-name-override-list '((rust "libtree-sitter-rust" "tree_sitter_rust")))
+
+(global-tree-sitter-mode)
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+
+(provide 'my-tree-sitter)
