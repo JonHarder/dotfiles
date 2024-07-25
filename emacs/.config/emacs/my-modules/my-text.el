d6bc8ebc00680d67e40078fda8023ab890b07d8e
Jon <jharder@kipsu.com>
2024-07-25 06:36:33 -0500

    emacs: new defun, cycle-capitalization

--- emacs/.config/emacs/my-modules/my-text.el
+++ emacs/.config/emacs/my-modules/my-text.el
@@ -8,4 +8,20 @@
 
 (add-hook 'text-mode-hook #'my-text-mode-hook)
 
+(defun cycle-capitalization ()
+  "Cycle capitalization of previous word.
+
+Capitalization schemes are:
+  - Regular capitalization: ensure only the first letter is capitalized
+  - All caps: Capitalize all letters of the word
+  - No caps: No Letters of the word are capitalized"
+  (interactive)
+  (save-excursion
+    (let ((word-begin (beginning-of-thing 'word)))
+      (goto-char word-begin)
+      (capitalize-word 1))))
+
+(evil-define-key '(insert normal) global-map
+  (kbd "M-c") #'cycle-capitalization)
+
 (provide 'my-text)
