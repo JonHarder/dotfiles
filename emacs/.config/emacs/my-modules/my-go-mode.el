66b1c77ef5df53adaaa68d3ef61c04504248ae9e
Jon Harder <jharder@kipsu.com>
2025-06-02 10:40:00 -0500

    emacs: elisp formatting updates in config.org

--- emacs/.config/emacs/my-modules/my-go-mode.el
+++ emacs/.config/emacs/my-modules/my-go-mode.el
@@ -6,6 +6,6 @@
 	 (getenv "PATH")))
 (defun my-go-mode-hook ()
   (when (eq major-mode 'go-mode)
-(gofmt-before-save)))
+	(gofmt-before-save)))
 (add-hook 'before-save-hook #'my-go-mode-hook)
 (provide 'my-go-mode)
