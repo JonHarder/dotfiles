ac76981ddf866d0eb9d3206bbb809dd0ca272591
Jon Harder <jharder@kipsu.com>
2025-09-20 11:49:39 -0500

    emacs: spacemacs theme

--- emacs/.config/emacs/my-modules/my-appearance.el
+++ emacs/.config/emacs/my-modules/my-appearance.el
@@ -1,4 +1,4 @@
-(defvar my-theme '(doom-themes . doom-dark+)
+(defvar my-theme '(spacemacs-theme . spacemacs-dark)
   "Cons of the symbols representing the theme package, and theme name.")
 (defvar my-font '(:name "JetBrains Mono" :size 170)
   "Font (:name string :size integer).")
