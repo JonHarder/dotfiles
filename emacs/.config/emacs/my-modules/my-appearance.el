c8e2beb6c8cfe574f9dd85b563fa59aaa7ceacea
Jon Harder <jharder@kipsu.com>
2025-10-13 10:31:58 -0500

    emacs: change theme to ef-autumn

--- emacs/.config/emacs/my-modules/my-appearance.el
+++ emacs/.config/emacs/my-modules/my-appearance.el
@@ -1,4 +1,4 @@
-(defvar my-theme '(modus-themes . modus-vivendi-tritanopia)
+(defvar my-theme '(ef-themes . ef-autumn)
   "Cons of the symbols representing the theme package, and theme name.")
 (defvar my-font '(:name "JetBrains Mono" :size 170)
   "Font (:name string :size integer).")
