e73ce6f347faf532896f8398be68523179986279
Jon Harder <jharder@kipsu.com>
2025-10-29 11:55:55 -0500

    emacs: wip try out doom-modeline-now-playing

--- emacs/.config/emacs/my-modules/my-modeline.el
+++ emacs/.config/emacs/my-modules/my-modeline.el
@@ -4,6 +4,11 @@
 (setq doom-modeline-hud t)
 (setq doom-modeline-major-mode-color-icon t)
 (setq doom-modeline-enable-word-count t)
+
+;; (straight-use-package 'doom-modeline-now-playing)
+;; (require 'doom-modeline-now-playing)
+;; (doom-modeline-now-playing-timer)
+
 (doom-modeline-mode 1)
 
 (provide 'my-modeline)
