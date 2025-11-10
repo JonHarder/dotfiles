85686852a553756053cf7737d8de6af2670662dc
Jon Harder <jharder@kipsu.com>
2025-11-10 10:17:57 -0600

    emacs: more calendar navigation

--- emacs/.config/emacs/my-modules/my-time.el
+++ emacs/.config/emacs/my-modules/my-time.el
@@ -1,6 +1,10 @@
 (with-eval-after-load 'calendar
   (keymap-set calendar-mode-map (kbd "h") #'calendar-backward-day)
-  (keymap-set calendar-mode-map (kbd "l") #'calendar-forward-day))
+  (keymap-set calendar-mode-map (kbd "l") #'calendar-forward-day)
+  (keymap-set calendar-mode-map (kbd "J") #'calendar-forward-month)
+  (keymap-set calendar-mode-map (kbd "K") #'calendar-backward-month)
+  (keymap-set calendar-mode-map (kbd "H") #'calendar-scroll-left)
+  (keymap-set calendar-mode-map (kbd "L") #'calendar-scroll-right))
 
 (setq calendar-date-style 'iso)
 
