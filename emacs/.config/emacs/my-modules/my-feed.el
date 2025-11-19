42fa46c8cbc9bd0013b9f8cf6aabaa10b8306de7
Jon Harder <jharder@kipsu.com>
2025-11-19 14:22:19 -0600

    emacs: update elfeed feeds

--- emacs/.config/emacs/my-modules/my-feed.el
+++ emacs/.config/emacs/my-modules/my-feed.el
@@ -2,9 +2,11 @@
 (setq elfeed-feeds
       '(("https://protesilaos.com/codelog.xml" code emacs)
         ("http://rss.desiringgod.org/" christianity)
+		("https://batsov.com/atom.xml" code)
         ("https://karthinks.com/index.xml" code emacs)
         ("https://www.thegospelcoalition.org/feed/" christianity)
         ("https://www.firstthings.com/rss/web-exclusives" christianity)
-		("https://irreal.org/blog/?feed=rss2" code emacs)))
+		("https://irreal.org/blog/?feed=rss2" code emacs)
+		("https://twingate.com/changelog.rss.xml" kipsu)))
 
 (provide 'my-feed)
