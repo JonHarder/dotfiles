fdce87513e705a0db0267712401ccab577e82d79
Jon Harder <jharder@kipsu.com>
2025-12-28 06:24:50 -0600

    emacs: removing flyspell in favor of jinx

--- emacs/.config/emacs/my-modules/my-text.el
+++ emacs/.config/emacs/my-modules/my-text.el
@@ -4,7 +4,9 @@
 
 (defun my-text-mode-hook ()
   (auto-fill-mode 1)
-  (flyspell-mode 1))
+  ;; disabling this in favor of jinx
+  ;; (flyspell-mode 1)
+)
 
 (add-hook 'text-mode-hook #'my-text-mode-hook)
 
