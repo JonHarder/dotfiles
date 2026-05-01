2a38b4bcc1146fa09453bf41baa31a5333520af9
Jon Harder <jharder@kipsu.com>
2026-05-01 13:57:45 -0500

    emacs: add pr-review package

--- emacs/.config/emacs/my-modules/my-git.el
+++ emacs/.config/emacs/my-modules/my-git.el
@@ -1,7 +1,10 @@
 (straight-use-package
  '(magit :type git :host github :repo "magit/magit"))
 
-(straight-use-package 'forge)
+(straight-use-package 'pr-review)
+(setq pr-review-forges-alist
+	  ;; default, reads api-host & username from ghub config
+      '(("github.com" . (github "api.github.com" "JonHarder"))))
 
 (straight-use-package 'diff-hl)
 (global-diff-hl-mode 1)
