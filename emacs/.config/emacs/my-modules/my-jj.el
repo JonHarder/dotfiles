200db9a67966a1d8222ef258691fb72d27646aa8
Jon Harder <jharder@kipsu.com>
2026-05-12 10:21:02 -0500

    emacs: fix org-node use-package statements

--- emacs/.config/emacs/my-modules/my-jj.el
+++ emacs/.config/emacs/my-modules/my-jj.el
@@ -1,6 +1,8 @@
+(use-package magit
+:straight t)
+
 (straight-use-package
  '(jj-mode :type git
            :host github
            :repo "bolivier/jj-mode.el"))
-(require 'magit)
 (provide 'my-jj)
