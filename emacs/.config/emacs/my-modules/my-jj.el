d3d37b30e07165dca0084c722ea9583e63401152
Jon Harder <jharder@kipsu.com>
2026-06-08 11:32:08 -0500

    emacs: Use majutsu for jujutsu package

--- emacs/.config/emacs/my-modules/my-jj.el
+++ emacs/.config/emacs/my-modules/my-jj.el
@@ -3,4 +3,9 @@
 
 (use-package jj-mode
   :straight (:host github :repo "bolivier/jj-mode.el"))
+
+(use-package majutsu
+  :straight (:host github :repo "0WD0/majutsu")
+  :commands (majutsu))
+
 (provide 'my-jj)
