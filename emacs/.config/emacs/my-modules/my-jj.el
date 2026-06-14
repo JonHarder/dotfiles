1fc6dd3a48a891722a875895eb109a2ade9ea846
Jon Harder <jharder@kipsu.com>
2026-06-14 07:23:53 -0500

    bind majutsu to global "C-c j"

--- emacs/.config/emacs/my-modules/my-jj.el
+++ emacs/.config/emacs/my-modules/my-jj.el
@@ -2,10 +2,12 @@
 :straight t)
 
 (use-package jj-mode
+  :disabled t
   :straight (:host github :repo "bolivier/jj-mode.el"))
 
 (use-package majutsu
   :straight (:host github :repo "0WD0/majutsu")
-  :commands (majutsu))
+  :commands (majutsu)
+  :bind ("C-c j" . majutsu))
 
 (provide 'my-jj)
