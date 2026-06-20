a2b706f0e5d03bc7e4a772c1605cada5675171db
Jon Harder <jharder@kipsu.com>
2026-06-19 22:11:47 -0500

    emacs: add security blogs to elfeed

--- emacs/.config/emacs/my-modules/my-feed.el
+++ emacs/.config/emacs/my-modules/my-feed.el
@@ -4,6 +4,8 @@
 	  '(("https://protesilaos.com/codelog.xml" code emacs)
 		("http://rss.desiringgod.org/" christianity)
 		("https://batsov.com/atom.xml" code)
+		("https://www.wiz.io/feed/rss.xml" security)
+		("https://rss.beehiiv.com/feeds/xgTKUmMmUm.xml" security)
 		("https://karthinks.com/index.xml" code emacs)
 		("https://www.thegospelcoalition.org/feed/" christianity)
 		("https://www.firstthings.com/rss/web-exclusives" christianity)
--- emacs/.config/emacs/my-modules/my-shells.el
+++ emacs/.config/emacs/my-modules/my-shells.el
@@ -32,6 +32,7 @@
 (add-to-list 'exec-path "/Users/jharder/.rustup/toolchains/stable-aarch64-apple-darwin/bin")
 (add-to-list 'exec-path "/opt/homebrew/opt/php@8.2/bin")
 (add-to-list 'exec-path "/opt/homebrew/opt/php@8.2/sbin")
+(add-to-list 'exec-path "/Users/jharder/.cargo/bin")
 
 (add-hook 'eshell-mode-hook
           (lambda ()
