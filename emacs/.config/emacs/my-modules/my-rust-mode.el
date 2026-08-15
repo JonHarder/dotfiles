fdd5ce00573c7cddd4405d75c628824b1c841cd7
Jon Harder <jharder@kipsu.com>
2026-08-15 16:10:53 -0500

    emacs: ensure rustfmt is using a modern language version

--- emacs/.config/emacs/my-modules/my-rust-mode.el
+++ emacs/.config/emacs/my-modules/my-rust-mode.el
@@ -1,5 +1,6 @@
 (straight-use-package 'rust-mode)
 (setq rust-format-on-save t)
+(setq rust-rustfmt-switches '("--edition=2024"))
 (add-hook 'rust-mode-hook
 		  (lambda () (prettify-symbols-mode)))
 (provide 'my-rust-mode)
