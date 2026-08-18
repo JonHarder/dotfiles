fba566a24ae50bf5ea11aad2940c820d586eca79
Jon Harder <jharder@kipsu.com>
2026-08-18 10:59:23 -0500

    emacs: add olivetti and bind to my toggles prefix: 'C-c t o'

--- emacs/.config/emacs/my-modules/my-toggles.el
+++ emacs/.config/emacs/my-modules/my-toggles.el
@@ -1,4 +1,10 @@
 (with-eval-after-load 'consult
   (global-set-key (kbd "C-c t t") #'consult-theme))
 (global-set-key (kbd "C-c t l") #'display-line-numbers-mode)
+
+(use-package olivetti
+  :straight t
+  :bind
+  ("C-c t o" . olivetti-mode))
+
 (provide 'my-toggles)
